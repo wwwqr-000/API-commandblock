eb5265b751b7b6e7f4c1951d14039478e604e6b7
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-10-30 01:02:50 +0100

    W.I.P curl

--- mod/src/main/java/com/wwwqr/ApiCmdBlock/commands/Wexec.java
+++ mod/src/main/java/com/wwwqr/ApiCmdBlock/commands/Wexec.java
@@ -1,11 +1,20 @@
 package com.wwwqr.ApiCmdBlock.commands;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+
+import com.wwwqr.ApiCmdBlock.ApiCmdBlock;
 
 public class Wexec {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -15,6 +24,47 @@ public class Wexec {
                     .executes(context -> {
                         String url =  StringArgumentType.getString(context, "url");
                         CommandSourceStack src = context.getSource();
+
+                        StringBuilder response = new StringBuilder();
+                        try {
+                            URL okUrl = new URL(url);
+                            String result = "";
+                            HttpURLConnection conn = (HttpURLConnection)okUrl.openConnection();
+                            conn.setRequestMethod("GET");
+
+                            int resCode = conn.getResponseCode();
+                            if (resCode != HttpURLConnection.HTTP_OK) {
+                                ApiCmdBlock.LOGGER.warn("wexec command did not return a HTTP_OK value. Aborted execution.");
+                                return Command.SINGLE_SUCCESS;
+                            }
+                            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                            String tmpLine;
+                            while ((tmpLine = in.readLine()) != null) {
+                                response.append(tmpLine);
+                            }
+                            in.close();
+                            result = response.toString();
+
+                            tmpLine = "";
+                            for (int i = 0; i < result.length(); i++) {
+                                char c = result.charAt(i);
+                                if (c != '\n') {
+                                    tmpLine += c;
+                                    continue;
+                                }
+                                src.getServer().getCommands().performPrefixedCommand(src, tmpLine);
+                                tmpLine = "";
+                            }
+                        }
+                        catch (MalformedURLException e) {
+                            ApiCmdBlock.LOGGER.error("wexec command url invalid", e);
+                            return Command.SINGLE_SUCCESS;
+                        }
+                        catch (IOException e) {
+                            ApiCmdBlock.LOGGER.error("wexec command IOException", e);
+                            return Command.SINGLE_SUCCESS;
+                        }
+
                         src.getServer().getCommands().performPrefixedCommand(src, "say goeie! (" + url + ")");
                         return Command.SINGLE_SUCCESS;
                     }))
